b8799690f31312c502a7f394a765d048d187a0d2
AchmadAkbarR <111202213959@mhs.dinus.ac.id>
2026-05-18 01:46:16 +0700

    fix entrypoint docker3

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,12 +4,21 @@ set -e
 # Sesuaikan port dengan Railway
 sed -i "s/listen 80;/listen ${PORT:-80};/g" /etc/nginx/nginx.conf
 
-# Migrasi database (pindahkan dari Dockerfile ke sini)
+# Package discover
+php artisan package:discover --ansi || true
+
+# Migrasi database
 php artisan migrate --force || true
 
 # PHP-FPM di background
 php-fpm -D
 
-# Nginx di foreground (supaya container tetap hidup)
+# Tunggu FPM siap
+sleep 2
+
+# Cek apakah FPM jalan
+echo "=== Checking PHP-FPM ==="
+ps aux | grep php-fpm
+
+# Nginx di foreground
 exec nginx -g "daemon off;"
-php artisan package:discover --ansi || true
