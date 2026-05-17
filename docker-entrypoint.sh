2ec6b2a57601afd27ab00f946e69a02fe9940c2a
AchmadAkbarR <111202213959@mhs.dinus.ac.id>
2026-05-18 01:53:57 +0700

    fix entrypoint docker6

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 
-# Hapus set -e dulu untuk debug
 sed -i "s/listen 80;/listen ${PORT:-80};/g" /etc/nginx/nginx.conf
-
-php artisan package:discover --ansi
-php artisan migrate --force
 
 echo "=== Starting PHP-FPM ==="
 php-fpm -D
-echo "=== PHP-FPM exit code: $? ==="
+echo "=== FPM done ==="
 sleep 2
 
 echo "=== PORT: ${PORT} ==="
